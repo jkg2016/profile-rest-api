6d48268c2ddab3a78a64c76864fe8a4fbf1dfffe
Jayman Gandhi <jayman.gandhi@gmail.com>
2020-08-07 16:29:29 -0400

    Removed the secret key.

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(p9o3wyp6j-i30*$*-%3h=o*z*knt7%h94j_kql9z$n#8xim0!'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework,authtoken',
+    'rest_framework.authtoken',
     'profiles_api',
 ]
 
